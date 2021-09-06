2caa33e628777c44f88f722d3904a0302f68b297
Kim Junseok <75918983+junseok-0505@users.noreply.github.com>
2021-09-06 18:23:50 +0900

    Add files via upload

---
+++ 이코테/이코테 1-3.py
@@ -0,0 +1,12 @@
+# 이코테 1-3.
+
+n, m = map(int, input().split())
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    if result < min(data):
+        result = min(data)
+        
+        
+print(result)
